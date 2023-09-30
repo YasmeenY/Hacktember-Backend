bd8728cedfd8a84efe6e1ab8db132a105ceaa96f
denis kadrich <dev4long@live.com>
2023-09-30 17:48:58 -0400

    seeded data

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import openai
 import os
 import uuid
 import requests
-import azure.cognitiveservices.speech as speechsdk
-from dotenv import load_dotenv
 
 openai.api_key = ""
 
@@ -33,30 +31,6 @@ def generate_reply(conversation: list) -> str:
     )
     return response["choices"][0]["message"]["content"]
 
-def generate_audio(text):
-    load_dotenv() 
-
-    speech_key =  os.getenv('SPEECH_KEY')
-    speech_reg = os.getenv('SPEECH_REGION')
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_reg)
-    audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-
-    # The language of the voice that speaks.
-    speech_config.speech_synthesis_voice_name='en-US-JennyNeural'
-
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
-    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
-
-    if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-        print("Speech synthesized for text [{}]".format(text))
-    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_synthesis_result.cancellation_details
-        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        if cancellation_details.error_details:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
 
 def generate_audio(text: str, output_path: str = "") -> str:
     voices = ELEVENLABS_ALL_VOICES
@@ -234,19 +208,6 @@ def listen(filename):
     # Return the audio file located at the given filename.
     return send_file(f"outputs/{filename}", mimetype="audio/mp3", as_attachment=False)
 
-
-@app.route('/listen', methods=['POST'])
-def listen():
-    text = request.get_json()[ 'text' ]
-    audio = generate_audio(text)
-    return jsonify({'text': reply})
-
-
-@app.route('/listen', methods=['POST'])
-def listen():
-    text = request.get_json()[ 'text' ]
-    audio = generate_audio(text)
-    return jsonify({'text': reply})
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
